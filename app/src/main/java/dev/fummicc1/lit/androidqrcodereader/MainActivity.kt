fefd7b768a11d183c47bdc5804b6d9113fb4a42a
fummicc1 <fumiya.tennis1@gmail.com>
2021-02-18 19:50:07 +0900

    read qrcode

--- app/src/main/java/dev/fummicc1/lit/androidqrcodereader/MainActivity.kt
+++ app/src/main/java/dev/fummicc1/lit/androidqrcodereader/MainActivity.kt
@@ -1,11 +1,32 @@
 package dev.fummicc1.lit.androidqrcodereader
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.google.zxing.integration.android.IntentIntegrator
+import dev.fummicc1.lit.androidqrcodereader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.button.setOnClickListener {
+            IntentIntegrator(this).initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        IntentIntegrator.parseActivityResult(requestCode, resultCode, data)?.let { result ->
+            val contents = result.contents
+            binding.textView.text = contents
+        }
     }
 }
