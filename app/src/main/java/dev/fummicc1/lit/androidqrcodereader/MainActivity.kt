d4968961b76b29d887368f30fa8e59897e08723a
fummicc1 <fumiya.tennis1@gmail.com>
2021-02-19 00:55:59 +0900

    fix:set binding.root in ContentView.

--- app/src/main/java/dev/fummicc1/lit/androidqrcodereader/MainActivity.kt
+++ app/src/main/java/dev/fummicc1/lit/androidqrcodereader/MainActivity.kt
@@ -3,9 +3,10 @@ package dev.fummicc1.lit.androidqrcodereader
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
+import android.util.Log
 import com.google.zxing.integration.android.IntentIntegrator
 import dev.fummicc1.lit.androidqrcodereader.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +14,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         binding.button.setOnClickListener {
+            Log.d("MainActivity#onCreate", "butoon is tapped.")
             IntentIntegrator(this).initiateScan()
         }
     }
